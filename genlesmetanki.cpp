6208560b4da1b92dcc40093c696b93eca52f9b2f
Kağan <kagansensazli@gmail.com>
2023-12-21 15:18:09 +0300

    comment qDebug() lines

--- genlesmetanki.cpp
+++ genlesmetanki.cpp
@@ -9,12 +9,12 @@ genlesmeTanki::genlesmeTanki(QObject *parent) : QObject{parent}
 
 void genlesmeTanki::genlesmeTankiHesap()
 {
-    qDebug() << "Çekilen veri:" << sonucDeger;
+    //qDebug() << "Çekilen veri:" << sonucDeger;
 
     konvertorluSonucDegeri = (int)(1 + 0.33 * debiDegeri * (ustBasincDegeri + 1) / (ustBasincDegeri - altBasincDegeri) / saltDegeri * 1000);
     konvertorsuzSonucDegeri = (int)(0.1 * debiDegeri * (ustBasincDegeri + 1) / (ustBasincDegeri - altBasincDegeri) / saltDegeri * 1000);
-    qDebug() << "Konvertörlü Sonuç Değeri:" << konvertorluSonucDegeri;
-    qDebug() << "Konvertörsüz Sonuç Değeri:" << konvertorsuzSonucDegeri;
+    //qDebug() << "Konvertörlü Sonuç Değeri:" << konvertorluSonucDegeri;
+    //qDebug() << "Konvertörsüz Sonuç Değeri:" << konvertorsuzSonucDegeri;
 
     if(sonucDeger != 0)
         debiDegeri = sonucDeger;
