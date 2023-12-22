60b7b9e6fd7d25cfa8c148347abb82cb54994b81
Kağan <kagansensazli@gmail.com>
2023-12-22 05:57:53 +0300

    added boiler capacity calculations

--- boylerkapasitesi.cpp
+++ boylerkapasitesi.cpp
@@ -5,4 +5,118 @@ boylerKapasitesi::boylerKapasitesi(QObject *parent) : QObject{parent}
 
 }
 
+void boylerKapasitesi::getIndex()
+{
+    //qDebug() << "index is:" << index;
+
+    if(index == "Apartman Dairesi")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 10;
+        banyoKatSayisi = 200;
+        dusKatSayisi = 250;
+        eviyeKatSayisi = 35;
+        camasirMakinasiKatSayisi = 70;
+        bulasikMakinasiKatSayisi = 40;
+        kullanmaKatSayisi = 0.30;
+        depolamaKatSayisi = 1.25;
+    }
+
+    else if(index == "Hastane")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 20;
+        banyoKatSayisi = 250;
+        dusKatSayisi = 250;
+        eviyeKatSayisi = 70;
+        camasirMakinasiKatSayisi = 75;
+        bulasikMakinasiKatSayisi = 300;
+        kullanmaKatSayisi = 0.25;
+        depolamaKatSayisi = 0.6;
+    }
+
+    else if(index == "Otel")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 30;
+        banyoKatSayisi = 250;
+        dusKatSayisi = 250;
+        eviyeKatSayisi = 70;
+        camasirMakinasiKatSayisi = 75;
+        bulasikMakinasiKatSayisi = 400;
+        kullanmaKatSayisi = 0.25;
+        depolamaKatSayisi = 0.80;
+    }
+
+    else if(index == "Fabrika")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 40;
+        banyoKatSayisi = 0;
+        dusKatSayisi = 750;
+        eviyeKatSayisi = 70;
+        camasirMakinasiKatSayisi = 0;
+        bulasikMakinasiKatSayisi = 200;
+        kullanmaKatSayisi = 0.40;
+        depolamaKatSayisi = 1;
+    }
+
+    else if(index == "Konut(Özel)")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 0;
+        banyoKatSayisi = 250;
+        dusKatSayisi = 250;
+        eviyeKatSayisi = 35;
+        camasirMakinasiKatSayisi = 70;
+        bulasikMakinasiKatSayisi = 40;
+        kullanmaKatSayisi = 0.30;
+        depolamaKatSayisi = 0.70;
+    }
+
+    else if(index == "Okul(Yatılı)")
+    {
+        ozelLavaboKatSayisi = 7.5;
+        genelLavaboKatSayisi = 50;
+        banyoKatSayisi = 0;
+        dusKatSayisi = 250;
+        eviyeKatSayisi = 35;
+        camasirMakinasiKatSayisi = 0;
+        bulasikMakinasiKatSayisi = 200;
+        kullanmaKatSayisi = 0.40;
+        depolamaKatSayisi = 1;
+    }
+
+    //qDebug() << "Kat Sayılar Sırasıyla:" << ozelLavaboKatSayisi << genelLavaboKatSayisi << banyoKatSayisi << dusKatSayisi << eviyeKatSayisi << camasirMakinasiKatSayisi << bulasikMakinasiKatSayisi << kullanmaKatSayisi << depolamaKatSayisi;
+
+}
+
+void boylerKapasitesi::boylerHesap()
+{
+    ozelLavaboSayisi = ozelLavaboKatSayisi * ozelLavaboSayisi;
+    genelLavaboSayisi = genelLavaboKatSayisi * genelLavaboSayisi;
+    banyoSayisi = banyoKatSayisi * banyoSayisi;
+    dusSayisi = dusKatSayisi * dusSayisi;
+    eviyeSayisi = eviyeKatSayisi * eviyeSayisi;
+    camasirMakinasiSayisi = camasirMakinasiKatSayisi * camasirMakinasiSayisi;
+    bulasikMakinasiSayisi = bulasikMakinasiKatSayisi * bulasikMakinasiSayisi;
+
+    kullanmaFaktoru = ((ozelLavaboSayisi + genelLavaboSayisi + banyoSayisi + dusSayisi + eviyeSayisi + camasirMakinasiSayisi + bulasikMakinasiSayisi) * kullanmaKatSayisi);
+    kullanmaFaktoruInt = (int)(kullanmaFaktoru + 0.5);
+
+    isitmaYuku = (kullanmaFaktoru * (60-10));
+    isitmaYukuInt = (int)(isitmaYuku + 0.5);
+
+    depolamaFaktoru = (depolamaKatSayisi * kullanmaFaktoru);
+    depolamaFaktoruInt = (int)(depolamaFaktoru + 0.5);
+
+    boylerIsitmaYuku = (isitmaYuku / 860);
+    boylerIsitmaYukuInt = (int)(boylerIsitmaYuku + 0.5);
+
+    //qDebug() << "Kullanma Faktörü:" << (int)(kullanmaFaktoru + 0.5);
+    //qDebug() << "Isıtma Yükü:" << (int)(isitmaYuku + 0.5);
+    //qDebug() << "Depolama Faktörü:" << (int)(depolamaFaktoru + 0.5);
+    //qDebug() << "Boyler Isıtma Yükü:" << (int)(boylerIsitmaYuku + 0.5);
+}
+
 
--- boylerkapasitesi.h
+++ boylerkapasitesi.h
@@ -7,10 +7,181 @@
 class boylerKapasitesi : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString index READ indexDegeri WRITE indexSet NOTIFY indexChanged)
+    Q_PROPERTY(double ozelLavaboSayisi READ ozelLavaboSayisiText WRITE ozelLavaboSayisiSet NOTIFY ozelLavaboSayisiChanged)
+    Q_PROPERTY(double genelLavaboSayisi READ genelLavaboSayisiText WRITE genelLavaboSayisiSet NOTIFY genelLavaboSayisiChanged)
+    Q_PROPERTY(double banyoSayisi READ banyoSayisiText WRITE banyoSayisiSet NOTIFY banyoSayisiChanged)
+    Q_PROPERTY(double dusSayisi READ dusSayisiText WRITE dusSayisiSet NOTIFY dusSayisiChanged)
+    Q_PROPERTY(double eviyeSayisi READ eviyeSayisiText WRITE eviyeSayisiSet NOTIFY eviyeSayisiChanged)
+    Q_PROPERTY(double camasirMakinasiSayisi READ camasirMakinasiSayisiText WRITE camasirMakinasiSayisiSet NOTIFY camasirMakinasiSayisiChanged)
+    Q_PROPERTY(double bulasikMakinasiSayisi READ bulasikMakinasiSayisiText WRITE bulasikMakinasiSayisiSet NOTIFY bulasikMakinasiSayisiChanged)
+    Q_PROPERTY(int kullanmaFaktoruInt READ kullanmaFaktoruText WRITE kullanmaFaktoruSet NOTIFY kullanmaFaktoruChanged)
+    Q_PROPERTY(int isitmaYukuInt READ isitmaYukuText WRITE isitmaYukuSet NOTIFY isitmaYukuChanged)
+    Q_PROPERTY(int depolamaFaktoruInt READ depolamaFaktoruText WRITE depolamaFaktoruSet NOTIFY depolamaFaktoruChanged)
+    Q_PROPERTY(int boylerIsitmaYukuInt READ boylerIsitmaYukuText WRITE boylerIsitmaYukuSet NOTIFY boylerIsitmaYukuChanged)
+
+
 public:
     explicit boylerKapasitesi(QObject *parent = nullptr);
 
+    QString indexDegeri() const{
+        return index;
+    }
+
+    double ozelLavaboSayisiText() const{
+        return ozelLavaboSayisi;
+    }
+
+    double genelLavaboSayisiText() const{
+        return genelLavaboSayisi;
+    }
+
+    double banyoSayisiText() const{
+        return banyoSayisi;
+    }
+
+    double dusSayisiText() const{
+        return dusSayisi;
+    }
+
+    double eviyeSayisiText() const{
+        return eviyeSayisi;
+    }
+
+    double camasirMakinasiSayisiText() const{
+        return camasirMakinasiSayisi;
+    }
+
+    double bulasikMakinasiSayisiText() const{
+        return bulasikMakinasiSayisi;
+    }
+
+    int kullanmaFaktoruText() const{
+        return kullanmaFaktoruInt;
+    }
+
+    int isitmaYukuText() const{
+        return isitmaYukuInt;
+    }
+
+    int depolamaFaktoruText() const{
+        return depolamaFaktoruInt;
+    }
+
+    int boylerIsitmaYukuText() const{
+        return boylerIsitmaYukuInt;
+    }
+
+    void indexSet(const QString &indexDegeri){
+        if(indexDegeri == index)
+            return;
+        index = indexDegeri;
+        emit indexChanged(index);
+    }
+
+    void ozelLavaboSayisiSet(const double &ozelLavaboSayisiText){
+        if(ozelLavaboSayisiText == ozelLavaboSayisi)
+            return;
+        ozelLavaboSayisi = ozelLavaboSayisiText;
+        emit ozelLavaboSayisiChanged(ozelLavaboSayisi);
+    }
+
+    void genelLavaboSayisiSet(const double &genelLavaboSayisiText){
+        if(genelLavaboSayisiText == genelLavaboSayisi)
+            return;
+        genelLavaboSayisi = genelLavaboSayisiText;
+        emit genelLavaboSayisiChanged(genelLavaboSayisi);
+    }
+
+    void banyoSayisiSet(const double &banyoSayisiText){
+        if(banyoSayisiText == banyoSayisi)
+            return;
+        banyoSayisi = banyoSayisiText;
+        emit banyoSayisiChanged(banyoSayisi);
+    }
+
+    void dusSayisiSet(const double &dusSayisiText){
+        if(dusSayisiText == dusSayisi)
+            return;
+        dusSayisi = dusSayisiText;
+        emit dusSayisiChanged(dusSayisi);
+    }
+
+    void eviyeSayisiSet(const double &eviyeSayisiText){
+        if(eviyeSayisiText == eviyeSayisi)
+            return;
+        eviyeSayisi = eviyeSayisiText;
+        emit eviyeSayisiChanged(eviyeSayisi);
+    }
+
+    void camasirMakinasiSayisiSet(const double &camasirMakinasiSayisiText){
+        if(camasirMakinasiSayisiText == camasirMakinasiSayisi)
+            return;
+        camasirMakinasiSayisi = camasirMakinasiSayisiText;
+        emit camasirMakinasiSayisiChanged(ozelLavaboSayisi);
+    }
+
+    void bulasikMakinasiSayisiSet(const double &bulasikMakinasiSayisiText){
+        if(bulasikMakinasiSayisiText == bulasikMakinasiSayisi)
+            return;
+        bulasikMakinasiSayisi = bulasikMakinasiSayisiText;
+        emit bulasikMakinasiSayisiChanged(bulasikMakinasiSayisi);
+    }
+
+    void kullanmaFaktoruSet(const int &kullanmaFaktoruText) {
+        if(kullanmaFaktoruText == kullanmaFaktoruInt)
+            return;
+        kullanmaFaktoruInt = kullanmaFaktoruText;
+        emit kullanmaFaktoruChanged(kullanmaFaktoruInt);
+    }
+
+    void isitmaYukuSet(const int & isitmaYukuText) {
+        if(isitmaYukuText == isitmaYukuInt)
+            return;
+        isitmaYukuInt = isitmaYukuText;
+        emit isitmaYukuChanged(isitmaYukuInt);
+    }
+
+    void depolamaFaktoruSet(const int &depolamaFaktoruText) {
+        if(depolamaFaktoruText == depolamaFaktoruInt)
+            return;
+        depolamaFaktoruInt = depolamaFaktoruText;
+        emit depolamaFaktoruChanged(depolamaFaktoruInt);
+    }
+
+    void boylerIsitmaYukuSet(const int & boylerIsitmaYukuText) {
+        if(boylerIsitmaYukuText == boylerIsitmaYukuInt)
+            return;
+        boylerIsitmaYukuInt = boylerIsitmaYukuText;
+        emit boylerIsitmaYukuChanged(boylerIsitmaYukuInt);
+    }
+
 signals:
+    void indexChanged(const QString & indexDegeri);
+    void ozelLavaboSayisiChanged(const double & ozelLavaboSayisi);
+    void genelLavaboSayisiChanged(const double & genelLavaboSayisi);
+    void banyoSayisiChanged(const double & banyoSayisi);
+    void dusSayisiChanged(const double & dusSayisi);
+    void eviyeSayisiChanged(const double & eviyeSayisi);
+    void camasirMakinasiSayisiChanged(const double & camasirMakinasiSayisi);
+    void bulasikMakinasiSayisiChanged(const double & bulasikMakinasiSayisi);
+    void kullanmaFaktoruChanged(const int & kullanmaFaktoruInt);
+    void isitmaYukuChanged(const int & isitmaYukuInt);
+    void depolamaFaktoruChanged(const int & depolamaFaktoruInt);
+    void boylerIsitmaYukuChanged(const int & boylerIsitmaYukuInt);
+
+public slots:
+    void getIndex();
+    void boylerHesap();
+
+private:
+    QString index;
+    double ozelLavaboKatSayisi, genelLavaboKatSayisi, banyoKatSayisi, dusKatSayisi, eviyeKatSayisi, camasirMakinasiKatSayisi, bulasikMakinasiKatSayisi;
+    double kullanmaKatSayisi, depolamaKatSayisi;
+    double ozelLavaboSayisi, genelLavaboSayisi, banyoSayisi, dusSayisi, eviyeSayisi, camasirMakinasiSayisi, bulasikMakinasiSayisi;
+    double kullanmaFaktoru, depolamaFaktoru, isitmaYuku, boylerIsitmaYuku;
+    int kullanmaFaktoruInt, depolamaFaktoruInt, isitmaYukuInt, boylerIsitmaYukuInt;
+
 };
 
 #endif // BOYLERKAPASITESI_H
