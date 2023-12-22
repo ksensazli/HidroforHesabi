a31997577e0161562e54730c46e6ebf80fd022d4
Kağan <kagansensazli@gmail.com>
2023-12-22 18:23:26 +0300

    define pisSuPompasi class

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include "hidrofor.h"
 #include "genlesmetanki.h"
 #include "boylerkapasitesi.h"
-
-double sonucDeger;
+#include "pissupompasi.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,12 +13,14 @@ int main(int argc, char *argv[])
     hidrofor h1;
     genlesmeTanki g1;
     boylerKapasitesi b1;
+    pisSuPompasi p1;
 
     QQmlApplicationEngine engine;
     QQmlContext* context = engine.rootContext();
     context->setContextProperty("hidrofor", &h1);
     context->setContextProperty("genlesme", &g1);
     context->setContextProperty("boyler"  , &b1);
+    context->setContextProperty("pisSu", &p1);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
